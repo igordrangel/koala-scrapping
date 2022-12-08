2a800e7446fcd514973b10dead0caeb74f5ac91d
Igor Rangel <igordrangel@gmail.com>
2022-12-08 14:45:59 -0300

    implementando proxy

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -67,3 +67,32 @@ test('Datatable Test', async () => {
 
   expect((await datatable.getData())?.length > 0).toEqual(true);
 });
+
+// test('Proxy Test', async () => {
+//   const google = new (class Google extends KoalaScrappingSystem<any> {
+//     constructor() {
+//       super({
+//         url: 'https://bemweb.bempromotora.com.br',
+//         proxyCredentials: {
+//           host: '41.65.168.43:1981',
+//           username: 'teste',
+//           password: 'teste',
+//         },
+//       });
+//     }
+
+//     public async search(wordSearch: string) {
+//       await this.init(false);
+//       await this.pasteValueInField('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input', wordSearch);
+//       await this.click('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]');
+
+//       const result = await this.getTextContentOnElement('//*[@id="result-stats"]');
+
+//       await this.closeDOM();
+
+//       return result;
+//     }
+//   })();
+
+//   expect(await google.search('test')).toContain('resultados');
+// });
