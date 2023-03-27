a279ffb3960c4cfdcb09892913aebb2c27764116
Igor Rangel <igordrangel@gmail.com>
2023-03-27 10:16:38 -0300

    implementando scrapOnFrame

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,29 +1,29 @@
 import { KoalaScrappingSystem } from '../KoalaScrappingSystem';
 jest.setTimeout(10000000);
-// test('Scrapping Google', async () => {
-//   const google = new (class Google extends KoalaScrappingSystem<any> {
-//     constructor() {
-//       super({
-//         url: 'https://google.com',
-//         loadMinimalist: true,
-//       });
-//     }
+test('Scrapping Google', async () => {
+  const google = new (class Google extends KoalaScrappingSystem<any> {
+    constructor() {
+      super({
+        url: 'https://google.com',
+        loadMinimalist: true,
+      });
+    }
 
-//     public async search(wordSearch: string) {
-//       await this.init();
-//       await this.pasteValueInField('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input', wordSearch);
-//       await this.click('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]');
+    public async search(wordSearch: string) {
+      await this.init();
+      await this.pasteValueInField('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input', wordSearch);
+      await this.click('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]');
 
-//       const result = await this.getTextContentOnElement('//*[@id="result-stats"]');
+      const result = await this.getTextContentOnElement('//*[@id="result-stats"]');
 
-//       await this.closeDOM();
+      await this.closeDOM();
 
-//       return result;
-//     }
-//   })();
+      return result;
+    }
+  })();
 
-//   expect(await google.search('test')).toContain('resultados');
-// });
+  expect(await google.search('test')).toContain('resultados');
+});
 
 // test('Download Test', async () => {
 //   const download = new (class Download extends KoalaScrappingSystem<any> {
@@ -48,25 +48,25 @@ jest.setTimeout(10000000);
 //   expect((await download.getFiles())?.length > 0).toEqual(true);
 // });
 
-// test('Datatable Test', async () => {
-//   const datatable = new (class Datatable extends KoalaScrappingSystem<any> {
-//     constructor() {
-//       super({
-//         url: 'https://datatables.net',
-//         loadMinimalist: true,
-//       });
-//     }
+test('Datatable Test', async () => {
+  const datatable = new (class Datatable extends KoalaScrappingSystem<any> {
+    constructor() {
+      super({
+        url: 'https://datatables.net',
+        loadMinimalist: true,
+      });
+    }
 
-//     public async getData() {
-//       await this.init();
-//       const items = await this.getDataFromTable<any>('//*[@id="example"]', 1000);
-//       await this.closeDOM();
-//       return items;
-//     }
-//   })();
+    public async getData() {
+      await this.init();
+      const items = await this.getDataFromTable<any>('//*[@id="example"]', 1000);
+      await this.closeDOM();
+      return items;
+    }
+  })();
 
-//   expect((await datatable.getData())?.length > 0).toEqual(true);
-// });
+  expect((await datatable.getData())?.length > 0).toEqual(true);
+});
 
 // test('Proxy Test', async () => {
 //   const google = new (class Google extends KoalaScrappingSystem<any> {
